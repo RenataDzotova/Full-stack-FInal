d7fa5c1f132410d09666beb464b628099ed02624
RenataDzotova <rdzotova@my.bcit.ca>
2023-04-08 16:34:16 -0700

    try

--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,23 @@ import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import { NextUIProvider } from "@nextui-org/react";
 import NavBar from "@/components/NavBar";
+import { useSSR } from "@nextui-org/react";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
+  const { isBrowser } = useSSR();
   return (
-    <NextUIProvider>
-      <SessionProvider session={session}>
-        <NavBar />
-        <Component {...pageProps} />
-      </SessionProvider>
-    </NextUIProvider>
+    <>
+      {isBrowser && (
+        <NextUIProvider>
+          <SessionProvider session={session}>
+            <NavBar />
+            <Component {...pageProps} />
+          </SessionProvider>
+        </NextUIProvider>
+      )}
+    </>
   );
 }
